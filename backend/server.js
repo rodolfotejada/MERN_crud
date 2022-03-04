e3ad13e9927af0ab63340978876fde3a6bb779a1
yanira castro <yaniracastro@yaniras-MacBook-Pro.local>
2022-03-04 09:43:38 -0400

    prepared deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const { CHAR_UPPERCASE_Z } = require('picomatch/lib/constants');
@@ -19,6 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 //Routes setup:
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 //Error handler setup:
 app.use(errorHandler);
